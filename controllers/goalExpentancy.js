7d3fb3a2e526faccb384c9f3a5868a0bf00f17ee
chinaza-max <mosesogbonna68@gmail.com>
2022-08-17 08:52:49 +0100

    update

--- controllers/goalExpentancy.js
+++ controllers/goalExpentancy.js
@@ -18,21 +18,34 @@ const calGoalExpectancy = (competitionName,date,homeId,awayId) => {
     //Team Home Goal Average For
     let THGA=result2.homeTeamTotalGoalScore/result2.homeTeamTotalGamePlayed
 
-    //Everton Home Goal Average Against
+    //Team Home Goal Average Against
     let THGAA=result2.homeTeamTotalGoalConceded/result2.homeTeamTotalGamePlayed
 
 
      //Team Away Goal Average For
      let TAGA=result2.awayTeamTotalGoalScore/result2.awayTeamTotalGamePlayed
 
-     //Everton Away Goal Average Against
+     //Team Away Goal Average Against
      let TAGAA=result2.awayTeamTotalGoalConceded/result2.awayTeamTotalGamePlayed
 
 
 
+    //Team’s Attack Strength
 
-    // let HomeTeam=
-    // let AwayTeam=
+    let HomeTeamStrength=THGA/GSHO
+    let AwayTeamStrength=TAGA/GSAO
+
+
+     //Team’s Defence Strength
+
+    let HomeTeamDefence=THGAA/GSAO
+    let AwayTeamDefence=TAGAA/GSHO
+
+
+   //Calculate Goal Expectancy
+
+   let HomeTeam=HomeTeamStrength*HomeTeamDefence*GSHO
+   let AwayTeam=AwayTeamStrength*AwayTeamDefence*GSAO
 }   
 
 
